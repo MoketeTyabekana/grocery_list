19f4e1b01ae829cd08b9b478c9c22777dcba467b
Mokete <emtyabekana@gmail.com>
2024-10-13 07:30:14 +0200

    retrieved our html element so that we can access them on our js file.

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+const container=document.getElementsByClassName('container');
+const pencil=document.getElementsByClassName('.material-icons');
+const userInput=document.getElementById('userInput');
+const items=document.getElementById('list-items');
+
