eb516a57aefd82a7ac15aef8cf98bd82ca514802
Mokete <emtyabekana@gmail.com>
2024-10-13 08:04:56 +0200

    Created an event listener to add a text style decoration of line-through when th eser clicks on the list item.

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
+const container = document.getElementsByClassName("container");
+const pencil = document.getElementsByClassName(".material-icons");
+const userInput = document.getElementById("userInput");
+const items = document.getElementById("list-items");
 
-
-const container=document.getElementsByClassName('container');
-const pencil=document.getElementsByClassName('.material-icons');
-const userInput=document.getElementById('userInput');
-const items=document.getElementById('list-items');
-
-pencil.addEventListener('click',function(){
-
-    items.innerHTML='';
-
+pencil.addEventListener("click", function () {
+  items.innerHTML = "";
 });
 
-userInput.addEventListener('keydown',function(event){
-
-   if(event.key=='Enter'){
-     addItem();
-   }
-
+userInput.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    addItem();
+  }
 });
 
-function addItem(){
-    let h2=document.createElement('h2');
-    h2.innerHTML='-'+ userInput.ariaValueMax.trim();
+function addItem() {
+  let h2 = document.createElement("h2");
+  h2.innerHTML = "-" + userInput.ariaValueMax.trim();
+
+  h2.addEventListener('click', function () {
+    h2.style.textDecoration = "line-through";
+  });
+
+  
 }
